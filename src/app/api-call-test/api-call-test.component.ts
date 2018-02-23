bde7e84ee3822a9d8c9f984191a228b94d2c3f76
Rane Fields <rane.fields@gmail.com>
2018-02-23 13:05:37 -0800

    Add test component with api call to node server

---
+++ src/app/api-call-test/api-call-test.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+@Component({
+  selector: 'app-api-call-test',
+  templateUrl: './api-call-test.component.html',
+  styleUrls: ['./api-call-test.component.scss']
+})
+export class ApiCallTestComponent implements OnInit {
+  private callResponse: Observable<string>;
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  ngOnInit() {
+    // TODO: Call should be made in a service, this is just a quick test
+    this.callResponse = this.http.get("/api/test", {responseType: 'text'});
+
+    // .subscribe to unwrap observable
+    this.callResponse
+      .subscribe(data => {
+        console.log(data);
+      });
+    }
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,16 @@
 import { Routes } from '@angular/router';
+
 import { SplashComponent } from './splash/splash.component';
 import { LogInComponent } from './log-in/log-in.component';
 import { RegisterComponent } from './register/register.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
+import { ApiCallTestComponent } from './api-call-test/api-call-test.component';
+
 export const ROUTES: Routes = [
   { path: '',   component: SplashComponent },
   { path: 'login',   component: LogInComponent },
   { path: 'register',   component: RegisterComponent },
+  { path: 'test',   component: ApiCallTestComponent },
   { path: '**',   component: NotFoundComponent },
 ];

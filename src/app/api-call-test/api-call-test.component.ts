c0bbb66843e30cb7ec6172963d66db554ff011cf
Rane Fields <rane.fields@gmail.com>
2018-02-26 15:19:49 -0800

    Fix some stuff

--- src/app/api-call-test/api-call-test.component.ts
+++ src/app/api-call-test/api-call-test.component.ts
@@ -8,19 +8,24 @@ import { Observable } from 'rxjs/Observable';
   styleUrls: ['./api-call-test.component.scss']
 })
 export class ApiCallTestComponent implements OnInit {
-  private callResponse: Observable<string>;
+  private testApiCallText: Observable<string>;
+  private testDbQueryTime: Observable<string>;
   constructor(
     private http: HttpClient
   ) { }
 
   ngOnInit() {
     // TODO: Call should be made in a service, this is just a quick test
-    this.callResponse = this.http.get("/api/test", {responseType: 'text'});
+    this.testApiCallText = this.http.get("/api/test", {responseType: 'text'});
 
     // .subscribe to unwrap observable
-    this.callResponse
+    this.testApiCallText
       .subscribe(data => {
         console.log(data);
       });
+    }
+
+    queryClick() {
+      this.testDbQueryTime = this.http.get("/api/test/db-time-query", {responseType: 'text'});
     }
 }

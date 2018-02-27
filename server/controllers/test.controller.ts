fe52df4b2eb1071f270c0ce6fab6af23698ee8b5
Rane Fields <rane.fields@gmail.com>
2018-02-26 16:42:35 -0800

    Fix some stuff

--- server/controllers/test.controller.ts
+++ server/controllers/test.controller.ts
@@ -4,18 +4,9 @@ import { Users } from "../models/Users";
 export class TestController {
   // Resolve => database time
   // Reject => err
-  async getTim() {
+  async getTime() {
     let response = await getConnection()
       .query("SELECT NOW()");
     return response[0].now;
-  }
-
-  async getTime() {
-    let admin = new Users();
-    admin.email = "adamtitus76@gmail.com";
-    admin.salt = "abc";
-    admin.hash = "blahblahblah";
-    let response = await getManager().save(admin);
-    return "hi";
   }
 }

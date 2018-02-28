b3760ce171bc83bd2b519eaccc4430eda875618b
Rane Fields <rane.fields@gmail.com>
2018-02-28 12:19:06 -0800

    Fix routing problems and make sure all routes are handled correctly

--- server/routes/api/test.routes.ts
+++ server/routes/api/test.routes.ts
@@ -11,4 +11,4 @@ router.get('/', TestController.getTestMessage);
 router.get('/db-time-query', TestController.getTimeFromDb);
 
 // GET /api/test/auth-test
-router.get('/auth-test', TestController.getTestUserData);
+router.get('/auth-test', auth, TestController.getTestUserData);

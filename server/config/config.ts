5448842f13f3127765ac5e33bd8b61a38c804830
Rane Fields <rane.fields@gmail.com>
2018-03-01 12:34:56 -0800

    Load all secrets from secrets.json instead of manually adding

--- server/config/config.ts
+++ server/config/config.ts
@@ -1,25 +1,18 @@
-let secrets = null;
-try {
-  secrets = require('../../secrets.json');
-}
-// JavaScript apparently can't do inner exceptions, so you get this instead.
-catch (e) {
-  console.error("Could not load secrets.json. This file is required in the root directory and may be missing. See documentation for details.\n");
-  throw e;
-}
-
 // Node settings
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 process.env.SERVER_PORT = process.env.SERVER_PORT || '3000';
 
-// Database settings
-process.env.POSTGRES_CONNECTION_STRING = secrets.POSTGRES_CONNECTION_STRING;
-process.env.ADMIN_EMAIL = secrets.ADMIN_EMAIL;
-process.env.ADMIN_PASSWORD = secrets.ADMIN_PASSWORD;
+// Load secrets.json
+let secrets = null;
+try {
+  secrets = require('../../secrets.json');
+}
+catch (e) {
+  console.error("Could not load secrets.json. This file is required in the root directory and may be missing. See documentation for details.\n");
+  throw e; // JavaScript apparently can't do inner exceptions, so you get this instead.
+}
 
-// JWT settings
-process.env.JWT_SECRET = secrets.JWT_SECRET;
-
-// SendGrid settings
-process.env.SENDGRID_API_KEY = secrets.SENDGRID_API_KEY;
-process.env.NOREPLY_EMAIL = secrets.NOREPLY_EMAIL;
+// Assign all secrets to environment variables
+Object.keys(secrets).forEach((key) => {
+  process.env[key] = secrets[key];
+});

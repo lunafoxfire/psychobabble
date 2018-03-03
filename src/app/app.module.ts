c67c00b93f7c0a003f7d533e9b9beb343b79ad21
Adam Titus <adamtitus76@gmail.com>
2018-03-02 17:37:35 -0800

    Finally get video uploading sorta maybe working.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgControl } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
 import { ROUTES } from './app.routes';
 import { AppComponent } from './app.component';
 

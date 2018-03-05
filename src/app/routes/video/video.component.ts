d5c3298b9b095c6e35a1234b79e171e282b668c2
Adam Titus <adamtitus76@gmail.com>
2018-03-05 11:56:23 -0800

    Successfull video upload.

--- src/app/routes/video/video.component.ts
+++ src/app/routes/video/video.component.ts
@@ -19,18 +19,16 @@ export class VideoComponent implements OnInit {
 
   }
 
-  private sendVideo(form: NgForm, file) {
-    let reader = new FileReader();
-    let _http = this.http;
-    reader.readAsArrayBuffer(file.files[0]);
-    reader.onloadend = function(e) {
-      let formData = new FormData();
-      formData.append('key', file.files[0].name);
-      formData.append('file', reader.result);
-      formData.append('acl', "public-read");
-      _http.post("http://epicodus-internship.s3.amazonaws.com/", formData).subscribe((res) => {
-        console.log(res);
-      })
-    }
+  private sendVideo(file) {
+    let formData = new FormData();
+    formData.append('key', "user1/"+file.files[0].name);
+    formData.append('file', file.files[0]);
+    formData.append('acl', "public-read");
+    formData.append('success_action_status', "200");
+    this.http.post("http://epicodus-internship.s3.amazonaws.com/", formData).subscribe((response) => {
+      console.log("response:" + response);
+    }, (error) => {
+      console.log("error:" + error)
+    })
   }
 }

471d65b9233b9ebbb3d8f85b85559a7fd674bff3
Rane Fields <rane.fields@gmail.com>
2018-03-07 19:30:49 -0800

    Encapsulate and clean up app startup for ease of understanding and use

--- server/bin/startup.ts
+++ server/bin/startup.ts
@@ -1,62 +1,41 @@
-#!/usr/bin/env node
 import * as http from 'http';
-import { app } from '../app';
+import { App } from '../app';
 import * as getDebug from 'debug';
 let debug = getDebug('expresstest:server');
 
+App.initAsync()
+  .then((app) => {
+    app.set('port', process.env.SERVER_PORT);
+    let server = http.createServer(app);
 
-app.then((app) => {
-  let port = normalizePort(process.env.SERVER_PORT);
-  app.set('port', port);
-  let server = http.createServer(app);
+    server.listen(process.env.SERVER_PORT);
+    server.on('error', onError);
+    server.on('listening', onListening);
+  })
+  .catch((err) => {
+    console.error("I just don't know what went wrong...");
+    console.error(err);
+  });
 
-  server.listen(port);
-  server.on('error', onError);
-  server.on('listening', onListening);
-
-
-  // Normalize a port into a number, string, or false.
-  function normalizePort(val) {
-  let port = parseInt(val, 10);
-  if (isNaN(port)) { // Is named pipe
-    return val;
+function onError(err) {
+  if (err.syscall !== 'listen') {
+    throw err;
   }
-  if (port >= 0) { // Is port number
-    return port;
+  // Friendly messages for listen errors
+  switch (err.code) {
+    case 'EACCES':
+      console.error(`Port ${process.env.SERVER_PORT} requires elevated privileges`);
+      process.exit(1);
+    break;
+    case 'EADDRINUSE':
+      console.error(`Port ${process.env.SERVER_PORT} is already in use`);
+      process.exit(1);
+    break;
+    default:
+      throw err;
   }
-  return false;
 }
 
-// Event listener for HTTP server "error" event.
-function onError(error) {
-if (error.syscall !== 'listen') {
-  throw error;
-}
-let bind = typeof port === 'string'
-? 'Pipe ' + port
-: 'Port ' + port;
-
-switch (error.code) { // Friendly messages for listen errors
-  case 'EACCES':
-  console.error(bind + ' requires elevated privileges');
-  process.exit(1);
-  break;
-  case 'EADDRINUSE':
-  console.error(bind + ' is already in use');
-  process.exit(1);
-  break;
-  default:
-  throw error;
-}
-}
-
-// Event listener for HTTP server "listening" event.
 function onListening() {
-let addr = server.address();
-let bind = typeof addr === 'string'
-? 'pipe ' + addr
-: 'port ' + addr.port;
-console.log('\nListening on ' + bind + '\n');
+  console.log(`Listening on port ${process.env.SERVER_PORT}...`);
 }
-
-})
--- server/tests/testing-tests/test-route.spec.ts
+++ server/tests/testing-tests/test-route.spec.ts
@@ -2,13 +2,13 @@ import 'mocha';
 import * as chai from 'chai';
 import { expect } from 'chai';
 import * as chaiHttp from 'chai-http';
-import { app } from './../../app';
+import { App } from './../../app';
 
 chai.use(chaiHttp);
 
 describe('test route', () => {
   it('should return 200 OK', async () => {
-    chai.request(await app)
+    chai.request(await App.initAsync())
       .get('/test-route')
       .then((res) => {
         expect(res).to.have.status(200);
@@ -21,7 +21,7 @@ describe('test route', () => {
 
 describe('test route 2', () => {
   it('should return 200 OK', async () => {
-    chai.request(await app)
+    chai.request(await App.initAsync())
       .get('/test-route-2')
       .then((res) => {
         expect(res).to.have.status(200);

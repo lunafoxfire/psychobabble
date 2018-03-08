4760b124a5fa0e7f5d4a07a461bcb1cd397176d6
Rane Fields <rane.fields@gmail.com>
2018-03-07 17:48:31 -0800

    Refactor app startup to be async

--- server/app.ts
+++ server/app.ts
@@ -22,61 +22,66 @@ console.log(
 );
 console.log(`Node environment: ${process.env.NODE_ENV}\n`);
 
-export let app = express();
+export let app = appStart();
 
-// Get database connection and initialize data
-console.log("Connecting to the database...");
-createConnection()
-  .then(async connection => {
-    await Role.syncRolesToDbAsync();
-    await User.generateDefaultAdminIfNoAdminAsync();
-    console.log("Successfully connected to the database.");
-  })
-  .catch((err) => console.error("Error connecting to the database!\n" + err));
+async function appStart() {
+  // Get database connection and initialize data
+  console.log("Connecting to the database...");
+  await createConnection()
+    .then(async connection => {
+      await Role.syncRolesToDbAsync();
+      await User.generateDefaultAdminIfNoAdminAsync();
+      console.log("Successfully connected to the database.");
+    })
+    .catch((err) => console.error("Error connecting to the database!\n" + err));
 
-if (process.env === 'development') { app.use(logger('dev')); }
+  let app = express();
+  if (process.env.NODE_ENV === 'development') { app.use(logger('dev')); }
 
-// Load middleware
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, '../dist'))); // Add Angular build folder to static files
+  // Load middleware
+  app.use(cors());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.static(path.join(__dirname, '../dist'))); // Add Angular build folder to static files
 
-// Load api routes with passport
-app.use(passport.initialize());
-app.use(router);
+  // Load api routes with passport
+  app.use(passport.initialize());
+  app.use(router);
 
-// Load Angular and let it handle view routes
-app.get('**', function(req, res, next) {
-  // If not AJAX request
-  if (!(req.xhr || req.headers.accept.indexOf('json') > -1)) {
-    let indexPage = path.join(__dirname, '../dist/index.html');
-    res.sendFile(indexPage, (err) => {
-      if (err) {
-        let errMsg = "index.html could not be found!";
-        console.error(errMsg);
-        res.status(500);
-        res.send(errMsg);
-      }
-    });
-  }
-  else {
-    next();
-  }
-});
+  // Load Angular and let it handle view routes
+  app.get('**', function(req, res, next) {
+    // If not AJAX request
+    if (!(req.xhr || req.headers.accept.indexOf('json') > -1)) {
+      let indexPage = path.join(__dirname, '../dist/index.html');
+      res.sendFile(indexPage, (err) => {
+        if (err) {
+          let errMsg = "index.html could not be found!";
+          console.error(errMsg);
+          res.status(500);
+          res.send(errMsg);
+        }
+      });
+    }
+    else {
+      next();
+    }
+  });
 
-// ===== Error handling ===== //
-// Catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  let err = new Error('The requested route could not be found');
-  err["status"] = 404;
-  next(err);
-});
+  // ===== Error handling ===== //
+  // Catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    let err = new Error('The requested route could not be found');
+    err["status"] = 404;
+    next(err);
+  });
 
-// Error handler
-app.use(function (err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({"message" : err.name + ": " + err.message});
-});
+  // Error handler
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({"message" : err.name + ": " + err.message});
+  });
+
+  return app;
+};
--- server/bin/startup.ts
+++ server/bin/startup.ts
@@ -4,17 +4,19 @@ import { app } from '../app';
 import * as getDebug from 'debug';
 let debug = getDebug('expresstest:server');
 
-let port = normalizePort(process.env.SERVER_PORT);
-app.set('port', port);
-let server = http.createServer(app);
 
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
+app.then((app) => {
+  let port = normalizePort(process.env.SERVER_PORT);
+  app.set('port', port);
+  let server = http.createServer(app);
+
+  server.listen(port);
+  server.on('error', onError);
+  server.on('listening', onListening);
 
 
-// Normalize a port into a number, string, or false.
-function normalizePort(val) {
+  // Normalize a port into a number, string, or false.
+  function normalizePort(val) {
   let port = parseInt(val, 10);
   if (isNaN(port)) { // Is named pipe
     return val;
@@ -27,32 +29,34 @@ function normalizePort(val) {
 
 // Event listener for HTTP server "error" event.
 function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-  let bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
+if (error.syscall !== 'listen') {
+  throw error;
+}
+let bind = typeof port === 'string'
+? 'Pipe ' + port
+: 'Port ' + port;
 
-  switch (error.code) { // Friendly messages for listen errors
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
+switch (error.code) { // Friendly messages for listen errors
+  case 'EACCES':
+  console.error(bind + ' requires elevated privileges');
+  process.exit(1);
+  break;
+  case 'EADDRINUSE':
+  console.error(bind + ' is already in use');
+  process.exit(1);
+  break;
+  default:
+  throw error;
+}
 }
 
 // Event listener for HTTP server "listening" event.
 function onListening() {
-  let addr = server.address();
-  let bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  console.log('\nListening on ' + bind + '\n');
+let addr = server.address();
+let bind = typeof addr === 'string'
+? 'pipe ' + addr
+: 'port ' + addr.port;
+console.log('\nListening on ' + bind + '\n');
 }
+
+})

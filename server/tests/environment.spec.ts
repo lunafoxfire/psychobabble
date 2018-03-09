67306986b84001e28f3e1663eee3e8bcf240c060
Rane Fields <rane.fields@gmail.com>
2018-03-09 13:37:45 -0800

    Use more correct syntax

--- server/tests/environment.spec.ts
+++ server/tests/environment.spec.ts
@@ -6,7 +6,7 @@ import { app } from './init.spec';
 describe("Environment variables", function() {
   describe("NODE_ENV", function() {
     it("should be definied", function() {
-      expect(process.env.NODE_ENV).to.not.equal(undefined);
+      expect(process.env.NODE_ENV).to.exist;
     });
     it("should be 'testing'", function() {
       expect(process.env.NODE_ENV).to.equal('testing');
@@ -15,7 +15,7 @@ describe("Environment variables", function() {
 
   describe("SERVER_PORT", function() {
     it("should be definied", function() {
-      expect(process.env.SERVER_PORT).to.not.equal(undefined);
+      expect(process.env.SERVER_PORT).to.exist;
     });
     it("should be a number", function() {
       let portNumber = parseInt(process.env.SERVER_PORT);
@@ -30,34 +30,34 @@ describe("Environment variables", function() {
 
   describe("DEFAULT_ADMIN_USERNAME", function() {
     it("should be definied", function() {
-      expect(process.env.DEFAULT_ADMIN_USERNAME).to.not.equal(undefined);
+      expect(process.env.DEFAULT_ADMIN_USERNAME).to.exist;
     });
     it("should not be empty", function() {
-      expect(process.env.DEFAULT_ADMIN_USERNAME).to.not.equal("");
+      expect(process.env.DEFAULT_ADMIN_USERNAME).to.not.be.empty;
     });
   });
 
   describe("DEFAULT_ADMIN_PASSWORD", function() {
     it("should be definied", function() {
-      expect(process.env.DEFAULT_ADMIN_PASSWORD).to.not.equal(undefined);
+      expect(process.env.DEFAULT_ADMIN_PASSWORD).to.exist;
     });
     it("should not be empty", function() {
-      expect(process.env.DEFAULT_ADMIN_PASSWORD).to.not.equal("");
+      expect(process.env.DEFAULT_ADMIN_PASSWORD).to.not.be.empty;
     });
   });
 
   describe("POSTGRES_HOST", function() {
     it("should be definied", function() {
-      expect(process.env.POSTGRES_HOST).to.not.equal(undefined);
+      expect(process.env.POSTGRES_HOST).to.exist;
     });
     it("should not be empty", function() {
-      expect(process.env.POSTGRES_HOST).to.not.equal("");
+      expect(process.env.POSTGRES_HOST).to.not.be.empty;
     });
   });
 
   describe("POSTGRES_PORT", function() {
     it("should be definied", function() {
-      expect(process.env.POSTGRES_PORT).to.not.equal(undefined);
+      expect(process.env.POSTGRES_PORT).to.exist;
     });
     it("should be a number", function() {
       let portNumber = parseInt(process.env.POSTGRES_PORT);
@@ -72,83 +72,83 @@ describe("Environment variables", function() {
 
   describe("POSTGRES_USERNAME", function() {
     it("should be definied", function() {
-      expect(process.env.POSTGRES_USERNAME).to.not.equal(undefined);
+      expect(process.env.POSTGRES_USERNAME).to.exist;
     });
     it("should not be empty", function() {
-      expect(process.env.POSTGRES_USERNAME).to.not.equal("");
+      expect(process.env.POSTGRES_USERNAME).to.not.be.empty;
     });
   });
 
   describe("POSTGRES_PASSWORD", function() {
     it("should be definied", function() {
-      expect(process.env.POSTGRES_PASSWORD).to.not.equal(undefined);
+      expect(process.env.POSTGRES_PASSWORD).to.exist;
     });
     it("should not be empty", function() {
-      expect(process.env.POSTGRES_PASSWORD).to.not.equal("");
+      expect(process.env.POSTGRES_PASSWORD).to.not.be.empty;
     });
   });
 
   describe("POSTGRES_DATABASE", function() {
     it("should be definied", function() {
-      expect(process.env.POSTGRES_DATABASE).to.not.equal(undefined);
+      expect(process.env.POSTGRES_DATABASE).to.exist;
     });
     it("should not be empty", function() {
-      expect(process.env.POSTGRES_DATABASE).to.not.equal("");
+      expect(process.env.POSTGRES_DATABASE).to.not.be.empty;
     });
   });
 
   describe("JWT_SECRET", function() {
     it("should be definied", function() {
-      expect(process.env.JWT_SECRET).to.not.equal(undefined);
+      expect(process.env.JWT_SECRET).to.exist;
     });
     it("should not be empty", function() {
-      expect(process.env.JWT_SECRET).to.not.equal("");
+      expect(process.env.JWT_SECRET).to.not.be.empty;
     });
   });
 
   describe("SENDGRID_API_KEY", function() {
     it("should be definied", function() {
-      expect(process.env.SENDGRID_API_KEY).to.not.equal(undefined);
+      expect(process.env.SENDGRID_API_KEY).to.exist;
     });
     it("should not be empty", function() {
-      expect(process.env.SENDGRID_API_KEY).to.not.equal("");
+      expect(process.env.SENDGRID_API_KEY).to.not.be.empty;
     });
   });
 
   describe("NOREPLY_EMAIL", function() {
     it("should be definied", function() {
-      expect(process.env.NOREPLY_EMAIL).to.not.equal(undefined);
+      expect(process.env.NOREPLY_EMAIL).to.exist;
     });
     it("should not be empty", function() {
-      expect(process.env.NOREPLY_EMAIL).to.not.equal("");
+      expect(process.env.NOREPLY_EMAIL).to.not.be.empty;
     });
     it("should be a valid email address")
   });
 
   describe("S3_BUCKET_NAME", function() {
     it("should be definied", function() {
-      expect(process.env.S3_BUCKET_NAME).to.not.equal(undefined);
+      expect(process.env.S3_BUCKET_NAME).to.exist;
     });
     it("should not be empty", function() {
-      expect(process.env.S3_BUCKET_NAME).to.not.equal("");
+      expect(process.env.S3_BUCKET_NAME).to.not.be.empty;
     });
   });
 
   describe("S3_ACCESS_KEY", function() {
     it("should be definied", function() {
-      expect(process.env.S3_ACCESS_KEY).to.not.equal(undefined);
+      expect(process.env.S3_ACCESS_KEY).to.exist;
     });
     it("should not be empty", function() {
-      expect(process.env.S3_ACCESS_KEY).to.not.equal("");
+      expect(process.env.S3_ACCESS_KEY).to.not.be.empty;
     });
   });
 
   describe("S3_SECRET_KEY", function() {
     it("should be definied", function() {
-      expect(process.env.S3_SECRET_KEY).to.not.equal(undefined);
+      expect(process.env.S3_SECRET_KEY).to.exist;
     });
     it("should not be empty", function() {
-      expect(process.env.S3_SECRET_KEY).to.not.equal("");
+      expect(process.env.S3_SECRET_KEY).to.not.be.empty;
     });
   });
 });

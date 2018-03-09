4ffd8e17bd8124ee02f30c796fdd223737d6f8d7
Rane Fields <rane.fields@gmail.com>
2018-03-09 13:34:46 -0800

    Remove test routes

--- server/routes/routes.ts
+++ server/routes/routes.ts
@@ -3,9 +3,7 @@ import { loadRoutes as loadApiRoutes } from './api/api.routes';
 
 export function loadRoutes() {
   let router = express.Router();
-
-  router.get('/test-route', (req, res) => {res.status(200).json({message: "hello route-testing world"})});
-  router.get('/test-route-2', (req, res) => {res.status(200).json({message: "beep boop"})});
+  
   router.use('/api', loadApiRoutes());
 
   return router;

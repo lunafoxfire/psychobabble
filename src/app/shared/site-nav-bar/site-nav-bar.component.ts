f19d5278fcd6a050b2c513e255cfd0ecd84ceab1
Adam Titus <adamtitus76@gmail.com>
2018-03-29 12:23:12 -0700

    Start on scrolling navbar.

--- src/app/shared/site-nav-bar/site-nav-bar.component.ts
+++ src/app/shared/site-nav-bar/site-nav-bar.component.ts
@@ -8,8 +8,31 @@ import { AuthService } from '../../auth/auth.service';
   templateUrl: './site-nav-bar.component.html'
 })
 
-export class SiteNavBarComponent {
+export class SiteNavBarComponent implements OnInit {
+  public scroll: boolean = false;
   constructor(
     public auth: AuthService
   ) { }
+
+  ngOnInit() {
+    window.onscroll = () => {
+      if(window.pageYOffset > 10) {
+        this.scroll = true;
+      } else {
+        this.scroll = false;
+      }
+    }
+  }
+
+  isScrolled() {
+    if(!this.auth.isAdmin) {
+      if(this.scroll) {
+        return "show-navbar";
+      } else {
+        return "hide-navbar";
+      }
+    } else {
+      return "show-navbar";
+    }
+  }
 }

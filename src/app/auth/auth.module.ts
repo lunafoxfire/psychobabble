c85ca52b2247053803758a2785cf7938958351a2
Rane Fields <rane.fields@gmail.com>
2018-04-04 16:55:12 -0700

    Move form components to shared

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { RouterModule } from '@angular/router';
 import { ROUTES } from './auth.routes';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from './../_imports/material.module';
+import { SharedModule } from './../shared/shared.module';
 
 import { ChangePasswordComponent } from './change-password/change-password.component';
 import { LoginComponent } from './login/login.component';
@@ -11,11 +12,8 @@ import { NewAdminComponent } from './new-admin/new-admin.component';
 import { RegisterComponent } from './register/register.component';
 import { ResetComponent } from './reset/reset.component';
 import { VerifyComponent } from './verify/verify.component';
-import { FormCardComponent } from './forms/form-card/form-card.component';
-import { FormHeaderComponent } from './forms/form-header/form-header.component';
-import { FormSubmitButtonComponent } from './forms/form-submit-button/form-submit-button.component';
-
 import { AuthService } from './auth.service';
+
 import { RegisterService } from './register/register.service';
 import { AuthGuardService } from './auth-guard.service';
 import { LoginGuardService } from './login-guard.service';
@@ -25,7 +23,6 @@ import {
   PasswordValidatorDirective,
   ConfirmPasswordValidatorDirective,
  } from './form-validator.directives';
-import { FormContentComponent } from './forms/form-content/form-content.component';
 
 @NgModule({
   declarations: [
@@ -38,15 +35,12 @@ import { FormContentComponent } from './forms/form-content/form-content.componen
     UsernameValidatorDirective,
     PasswordValidatorDirective,
     ConfirmPasswordValidatorDirective,
-    FormCardComponent,
-    FormHeaderComponent,
-    FormSubmitButtonComponent,
-    FormContentComponent,
   ],
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
+    SharedModule,
     MaterialModule,
     RouterModule.forChild(ROUTES),
   ],
